dc8d2afea99058a503ea872f569688cb0a0edc6b
Royce Le <royle.com>
2017-07-18 00:31:26 -0700

    README details & form additions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # usrTestData
+-------------
+This application is meant to create user test data.
+
+https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms
--- myScript.js
+++ myScript.js
@@ -1,22 +1,5 @@
-// JavaScript source code
-/* STOP
-var settings = {
-    "async": true,
-    success: function(result) {
-        alert("IT WORKS YA WOO");
-    },
-    "crossDomain": true,
-    "origin": "https://test16host.openapi.starbucks.com",
-    "url": "https://test16host.openapi.starbucks.com/identifier/v1/xid/AE72AABA-439F-45C8-A56C-D07DD996C509/userId",
-    "method": "GET",
-    "headers": {
-        "accept": "application/json",
-        "cache-control": "no-cache",
-        "postman-token": "ac01f860-8f58-ce4c-a4c4-81dcde8e4f36"
-    }
-}
-STOP*/
-document.domain = 'https://starbucks.com';
+
+/*document.domain = 'https://starbucks.com';
 
 var settings = {
   "async": true,
@@ -36,20 +19,4 @@ $(document).ready(function (response) {
         console.log(response);
     })
 })
-
-//$(document).ready(function () {
-//    console.log("please work");
-
-    /*$("#pp").click(function () {
-        alert("The paragraph was clicked.");
-    });*/
-
-    /*STOP
-    $("#pp").click(function () {
-        alert("wtf");
-        $.ajax(settings).then(function (response) {
-            alert("PLEASE");
-        });
-    })
-})
-STOP*/
+*/
